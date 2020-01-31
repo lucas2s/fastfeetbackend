1b1c5a2cc149a036250334f63dcd2b8f8e8edb9c
Lucas Silva Sartori <sartori.s.lucas@gmail.com>
2020-01-31 18:05:13 -0300

    Criação Model, Controller Recipient

---
+++ src/app/controllers/RecipientController.js
@@ -0,0 +1,85 @@
+import * as Yup from 'yup';
+import Recipient from '../models/Recipient';
+
+class RecipientController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      street: Yup.string().required(),
+      number: Yup.number().required(),
+      complement: Yup.string().required(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      zipcode: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Falha na validação dos campos' });
+    }
+
+    const recipient = await Recipient.create(req.body);
+
+    return res.json({
+      recipient,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      street: Yup.string(),
+      number: Yup.number(),
+      complement: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      zipcode: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Falha na validação dos campos' });
+    }
+
+    const { id } = req.params;
+
+    const recipient = await Recipient.findByPk(id);
+
+    if (!recipient) {
+      return res.status(400).json({ error: 'Destinatário enviado Inválido' });
+    }
+
+    const recipientRes = await recipient.update(req.body);
+
+    return res.json({
+      recipientRes,
+    });
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    const recipient = await Recipient.findByPk(id);
+
+    if (!recipient) {
+      return res.status(400).json({ error: 'Destinatário enviado Inválido' });
+    }
+
+    await recipient.destroy();
+
+    return res.json({
+      message: 'Destinatário deletado com Sucesso',
+    });
+  }
+
+  async indexByPk(req, res) {
+    const { id } = req.params;
+    const recipient = await Recipient.findByPk(id);
+
+    if (!recipient) {
+      return res.status(400).json({ error: 'Destinatário enviado Inválido' });
+    }
+
+    return res.json({
+      recipient,
+    });
+  }
+}
+
+export default new RecipientController();
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
+import RecipientController from './app/controllers/RecipientController';
 
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.post('/recipients', RecipientController.store);
+routes.put('/recipients/:id', RecipientController.update);
+routes.get('/recipients/:id', RecipientController.indexById);
+routes.delete('/recipients/:id', RecipientController.delete);
 
 export default routes;
