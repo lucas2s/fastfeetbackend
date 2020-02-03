d5ad56f6af2ec06428cfe0330557dbff67d5f87a
Lucas Silva Sartori <sartori.s.lucas@gmail.com>
2020-02-03 15:25:13 -0300

    Upload de arquivos - avatar

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 import RecipientController from './app/controllers/RecipientController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 
@@ -14,5 +17,9 @@ routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
 routes.delete('/recipients/:id', RecipientController.delete);
 routes.get('/recipients/:id', RecipientController.indexById);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 export default routes;
