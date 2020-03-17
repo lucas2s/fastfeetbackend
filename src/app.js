3df49c51d6afbe56d7802723678c23af4f1e609d
Lucas Silva Sartori <sartori.s.lucas@gmail.com>
2020-03-16 22:59:53 -0300

    Add cors para acesso via aplicação REACT

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 
+import cors from 'cors';
 import express from 'express';
 import routes from './routes';
 
@@ -13,6 +14,7 @@ class App {
   }
 
   middlewares() {
+    this.server.use(cors());
     this.server.use(express.json());
   }
 
